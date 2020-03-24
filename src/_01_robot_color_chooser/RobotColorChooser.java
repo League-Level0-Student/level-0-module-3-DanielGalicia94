e005c0a26a8cd4afcdb587cee7ed5a6af469a1d7
root <root@da3ae49ab364>
2020-03-24 21:32:30 +0000

    crazycatlady

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,18 +4,57 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
+		Robot dan = new Robot();
+		dan.setPenWidth(10);
+		dan.penDown();
+		dan.setSpeed(100);
+		int sides = 3;
+		int lenght = 0;
+		for (int i = 0; i < 90; i++) {
+			
 		
-		//3. Ask the user what color they would like the robot to draw
-		
+		/*3. Ask the user what color they would like the robot to draw
+	String color = JOptionPane.showInputDialog("What color do you want your robot");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+	if (color.equals("red")) {
+		dan.setPenColor(255, 0, 0);
+	} else if (color.equals("green")){
+		dan.setPenColor(0,255, 0);
+	} else if (color.equals("blue")) {
+		dan.setPenColor(0, 0, 255);
+	} else if (color.equals("yellow")) {
+		dan.setPenColor(255, 255, 0);
+	} else if (color.equals("purple")) {
+		dan.setPenColor(255, 0, 255);
+	} else if (color.equals("orange")) {
+		dan.setPenColor(255, 165, 0);
+	} else if (color.equals("white")) {
+		dan.setPenColor(255, 255, 255);
+	} else if (color.equals("black")) {
+		dan.setPenColor(0, 0, 0);
+	} else if (color.equals("")) {
+	    dan.setRandomPenColor(); 
         //6. If the user doesn’t enter anything, choose a random color
-
+	} else {
+		JOptionPane.showMessageDialog(null, "I don't recognise the color");
+	} */
+			dan.setRandomPenColor();
+		
+		for (int j = 0; j < sides; j++) {
+		dan.turn(360/sides);
+		dan.move(50);
+		}
+		sides ++;
+		lenght = lenght*2;
+		
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
@@ -23,5 +62,5 @@ public class RobotColorChooser {
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 
 
-	}
+	}}
 }
